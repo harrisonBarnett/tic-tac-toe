ba046d5f0ee49ce89417654e8d72024bd1e8152f
harrisonBarnett <h.alexander.barnett@gmail.com>
2021-06-26 10:50:40 -0400

    Dynamic form for initializing players

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const gameBoard = (() => {
             // cells.forEach(cell) => {
             //     cell.remove();
             // }
-            console.log('cleared the board');
+            console.log('cleared the current board');
         
         }
         // append rows
@@ -43,24 +43,97 @@ const gameBoard = (() => {
         resetBtn.className = "board-reset-btn";
         resetBtn.innerHTML = "RESET";
         resetBtn.onclick = function() {clearBoard()};
+        const quitBtn = document.createElement('button');
+        quitBtn.className = "board-quit-btn";
+        quitBtn.innerHTML = "QUIT";
         btnContainer.append(resetBtn);
+        btnContainer.append(quitBtn);
         board.append(btnContainer);
         
     };
 
     return {displayBoard};
 })();
+// gameBoard.displayBoard();
 
-gameBoard.displayBoard();
+const Player = function(name, icon) {
+    this.icon = icon;
+    this.name = name;
+    this.score = 0; 
+    this.playerInfo = () => console.log(name, score);
+};
 
-const displayController = (() => {
-    // set event listener to react to player clicks
-    document.addEventListener('click', function(e) {
-        if(e.target.classList.contains('cell')) {
-            e.target.innerHTML = "<span>X</span>";
+const setup = (() => {
+    const body = document.querySelector('body');
+    //display startup form
+    function form() {
+        const formContainer = document.createElement('div');
+        formContainer.className = "start-form-container";
+
+        function playerSetup(playerCSS, playerTitle) {
+            const playerContainer = document.createElement('div');
+            playerContainer.className = "player-container " + playerCSS + "-container";
+
+            // input name
+            const name = document.createElement('input');
+            name.className = playerCSS + "-input";
+            name.name = playerCSS + "-input";
+            const nameLabel = document.createElement('label');
+            nameLabel.innerHTML = playerTitle;
+            nameLabel.htmlFor = playerCSS + "-input"; 
+            
+            // options container (X or O player pieces)
+            const optionsContainer = document.createElement('div');
+            optionsContainer.className = playerCSS + "-options";
+
+            const optionX = document.createElement('input');
+            optionX.type = 'checkbox';
+            optionX.className = playerCSS + "-optionX";
+            optionX.name = playerCSS + "-optionX";
+            const optionXlabel = document.createElement('label');
+            optionXlabel.htmlFor = playerCSS + "-optionX";
+            optionXlabel.innerHTML = "X";
+            optionsContainer.append(optionXlabel, optionX);
+
+            const optionO = document.createElement('input');
+            optionO.type = 'checkbox';
+            optionO.className = playerCSS + "-optionO";
+            optionO.name = playerCSS + "-optionO";
+            const optionOlabel = document.createElement('label');
+            optionOlabel.htmlFor = playerCSS + "-optionO";
+            optionOlabel.innerHTML = "O";
+            optionsContainer.append(optionOlabel, optionO);
+
+
+            playerContainer.append(nameLabel, name, optionsContainer);
+            formContainer.append(playerContainer);
         }
-    });
+        playerSetup('player-1', "Player One");
+        playerSetup('player-2', "Player Two");
+
+        body.append(formContainer);
+    }
+    return {form};
 })();
 
+// initialize gameboard functionality and game logic
+const displayController = (() => {
+    // set event listener to react to player clicks
+    function initCells() {
+        document.addEventListener('click', function(e) {
+            if(e.target.classList.contains('cell')) {
+                e.target.innerHTML = "<span>X</span>";
+            }
+        });
+    }
+    // quit button resets the entire game state
+    const quitBtn = document.querySelector('.board-quit-btn');
+    quitBtn.addEventListener('click', function() {
+        console.log('reset game state');
+    });
+
+    return {initCells};
+})();
+// displayController.initCells();
 
 
